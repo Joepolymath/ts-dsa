d092268dafd67ebed213fc29dd668fa39e5a4099
Joshua_Ajagbe <joshua@equally.ai>
2024-01-10 15:26:22 +0100

    init structure and stack

---
+++ src/dataStructures/index.ts
@@ -0,0 +1,5 @@
+import Stack1 from './stack/stack1';
+
+export default {
+  stackWithArray: Stack1,
+};
---
+++ src/dataStructures/stack/stack1.ts
@@ -0,0 +1,41 @@
+// using array under the hood
+class Stack1 {
+  private data: any[];
+  private size: number;
+
+  constructor() {
+    this.data = [];
+    this.size = 0;
+  }
+
+  public push(value: any) {
+    this.size++;
+    return this.data.push(value);
+  }
+
+  public pop() {
+    this.size--;
+    return this.data.pop();
+  }
+
+  public length() {
+    return this.size;
+  }
+
+  //   view complete stack
+  public getBuffer() {
+    return this.data.slice();
+  }
+
+  //   check if stack is empty
+  public isEmpty() {
+    return this.data.length === 0;
+  }
+
+  //   Peeking means returning the last added element to the stack without removing it from stack.
+  public peek() {
+    return this.data[this.size - 1];
+  }
+}
+
+export default Stack1;
