ff3d2b7bd71f4db62a4c07d2418a1096fcab313c
Joshua_Ajagbe <joshuaajagbe96@gmail.com>
2024-01-15 01:19:55 +0100

    leetcode 242 solved

---
+++ src/leet/easy/242.ts
@@ -0,0 +1,31 @@
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const memory: any = {};
+  const memory2: any = {};
+
+  for (let element of s) {
+    if (memory[element]) {
+      memory[element] = memory[element] + 1;
+    } else {
+      memory[element] = 1;
+    }
+  }
+  for (let element of t) {
+    if (memory2[element]) {
+      memory2[element] = memory2[element] + 1;
+    } else {
+      memory2[element] = 1;
+    }
+  }
+
+  const keys = Object.keys(memory);
+  for (let key of keys) {
+    if (memory[key] !== memory2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
