cadf7126a253607aa6466d9d46e10e9cf3719c2c
Joshua_Ajagbe <joshuaajagbe96@gmail.com>
2024-01-18 02:16:22 +0100

    leetcode 121 solved with 2 pointers technique

--- src/leet/easy/121.ts
+++ src/leet/easy/121.ts
@@ -34,3 +34,23 @@ function maxProfit(prices: number[]): number {
   }
   return profit;
 }
+
+// a better implementation using 2 pointers technique
+function maxProfit2(prices: number[]): number {
+  let max = 0;
+  let l = 0;
+  let r = 1;
+
+  while (r < prices.length) {
+    if (prices[l] < prices[r]) {
+      let profit = prices[r] - prices[l];
+      if (profit > max) {
+        max = profit;
+      }
+    } else {
+      l = r;
+    }
+    r++;
+  }
+  return max;
+}
