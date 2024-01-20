e3413433ca21eed876a0c66152ede31941f0b86d
Joshua_Ajagbe <joshuaajagbe96@gmail.com>
2024-01-20 16:53:58 +0100

    leetcode 347 solution from uche

--- src/leet/medium/347.ts
+++ src/leet/medium/347.ts
@@ -1,36 +1,52 @@
-function topKFrequent(nums: number[], k: number): number[] | undefined {
+function topKFrequent(nums: number[], k: number): number[] {
   const hash: {
     [key: number]: number;
   } = {};
-
-  const freq: number[][] = Array.apply(null, Array(nums.length + 1)).map(
+  const frequency: number[][] = Array.apply(null, Array(nums.length + 1)).map(
     () => []
   );
 
-  nums.forEach((item) => {
-    if (hash[item]) {
-      hash[item]++;
+  nums.forEach((num) => {
+    if (hash[num]) {
+      hash[num]++;
     } else {
-      hash[item] = 1;
+      hash[num] = 1;
     }
   });
 
   Object.keys(hash).forEach((item: any) => {
-    const key = Number(item);
-    const value = hash[item];
-    freq[value].push(key);
+    const element = Number(item);
+    frequency[hash[item]].push(element);
   });
 
-  const res: number[] = [];
-
-  for (let i = freq.length - 1; i >= 0; i--) {
-    const element = freq[i];
-    for (let j = 0; j < element.length; j++) {
-      const second = element[j];
-      res.push(Number(second));
-      if (res.length == k) {
-        return res;
+  // preparing and finalizing result
+  const result = [];
+  for (let i = frequency.length - 1; i >= 0; i--) {
+    for (let j = 0; j < frequency[i].length; j++) {
+      result.push(frequency[i][j]);
+      if (result.length === k) {
+        return result;
       }
     }
   }
+  return result;
+}
+
+function topKFrequent2(nums: number[], k: number): number[] {
+  let m = new Map();
+
+  nums.forEach((num) => {
+    let a = m.get(num);
+    if (a) {
+      m.set(num, a + 1);
+    } else {
+      m.set(num, 1);
+    }
+  });
+
+  const sortedArr = Array.from(
+    new Map([...m.entries()].sort((a, b) => b[1] - a[1])).keys()
+  );
+
+  return sortedArr.slice(0, k);
 }
