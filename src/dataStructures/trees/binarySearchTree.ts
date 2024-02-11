f10e4d812ac76ae352b4a28a98dc1edef814e325
Joshua_Ajagbe <joshuaajagbe96@gmail.com>
2024-02-12 00:45:50 +0100

    binary search tree with insert method

---
+++ src/dataStructures/trees/binarySearchTree.ts
@@ -0,0 +1,43 @@
+class TreeNode<T> {
+  value: T;
+  left: TreeNode<T> | null;
+  right: TreeNode<T> | null;
+  constructor(value: T) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree<T> {
+  root: TreeNode<T> | null;
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value: T) {
+    const newNode = new TreeNode<T>(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+    while (true) {
+      if (current.value === value) return null;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
